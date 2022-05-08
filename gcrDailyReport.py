387d73b53d902c1101276870d1a9d2ef6def0ef9
MohammedAzim Shaikh <azim.music9@gmail.com>
2022-05-08 16:51:40 +0530

    Updated on 2022-05-08 16:51:39

--- gcrDailyReport.py
+++ gcrDailyReport.py
@@ -331,10 +331,12 @@ elif (sidebarContent == "Milestone Leaderboard"):
             #st.write("Developed & Managed By : MohammedAzim Shaikh")
             
     elif (textInput == "azim@ljku.edu.in" or textInput == "azim" or textInput == "azim@ljp" or textInput == "jayniyati2233@gmail.com" or textInput == "kirtankp1902@gmail.com"):
-        st.snow()
-        st.info("Dear Facilitator, Please find the above progress for LJ Institute of Engineering & Technology, Ahmedabad.")
-        st.success('This is a success message!')
-        st.warning('This is a warning')
+        with st.spinner('Wait for it...'):
+            time.sleep(5)
+        st.success('Done!')
+        #st.snow()
+        #st.info("Dear Facilitator, Please find the above progress for LJ Institute of Engineering & Technology, Ahmedabad.")
+        
 
     elif (textInput != "" and status == False):
         st.error("Sorry, we won't be able to show you the Milestone Achievers unless and until you are a Participant under GCRF Program LJ Institute of Engineering & Technology, Ahmedabad.")
