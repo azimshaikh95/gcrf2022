6698c1984269929b286c503d68a15738d5851423
MohammedAzim Shaikh <azim.music9@gmail.com>
2022-05-12 20:17:07 +0530

    Updated on 2022-05-12 20:17:07

--- gcrDailyReport.py
+++ gcrDailyReport.py
@@ -490,7 +490,7 @@ elif (sidebarContent == "Admin Zone"):
         st.markdown('<b class="big-font">Facilitator Milestone Status</b>', unsafe_allow_html=True)
 
         tq, ts, tp = fmsc(500, 250)
-        st.subheader("You have completed " + str(tq) + " Quests and " + str(ts) +" Skill Badges.")
+        # st.subheader("You have completed " + str(tq) + " Quests and " + str(ts) +" Skill Badges.")
         if(tq >= 500 and ts >= 250):
             st.balloons()
 
